85a6eb0902eb4abb0702b7124fb827e737937def
Taavi Allingu <taavi.allingu@gmail.com>
2025-06-07 16:18:13 +0300

    fix cookie

--- src/pages/CookieClicker.vue
+++ src/pages/CookieClicker.vue
@@ -5,8 +5,20 @@ const cookies = ref(0);
 const buildings = ref([
     { name: 'Cursor', price: 15, cps: 0.1, count: 0},
     { name: 'Grandma', price: 100, cps: 1, count: 0},
-    { name: 'Farm', price: 1000, cps: 10, count: 0},
-    { name: 'Factory', price: 10_000, cps: 100, count: 0},
+    { name: 'Farm', price: 1000, cps: 8, count: 0},
+    { name: 'Mine', price: 12000, cps: 47, count: 0},
+    { name: 'Factory', price: 130000, cps: 260, count: 0},
+    { name: 'Bank', price: 1400000, cps: 1400, count: 0},
+    { name: 'Temple', price: 20000000, cps: 7800, count: 0},
+    { name: 'Wizard Tower', price: 330000000, cps: 44000, count: 0},
+    { name: 'Shipment', price: 5100000000, cps: 260000, count: 0},
+    { name: 'Alchemy Lab', price: 75000000000, cps: 1600000, count: 0},
+    { name: 'Portal', price: 1000000000000, cps: 10000000, count: 0},
+    { name: 'Time Machine', price: 14000000000000, cps: 65000000, count: 0},
+    { name: 'Antimatter Condenser', price: 170000000000000, cps: 430000000, count: 0},
+    { name: 'Prism', price: 2100000000000000, cps: 2900000000, count: 0},
+    { name: 'Chancemaker', price: 26000000000000000, cps: 21000000000, count: 0},
+    { name: 'Fractal Engine', price: 310000000000000000, cps: 150000000000, count: 0},
 ]);
 
 function buyBuilding(building){
